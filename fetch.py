3959e60a22e1398d251e24a7d173b762639a01b8
Tom M. Ragonneau <33006763+ragonneau@users.noreply.github.com>
2023-01-04 09:53:39 +0800

    Update fetch.py

--- fetch.py
+++ fetch.py
@@ -7,55 +7,63 @@ import pypistats
 from github import Github
 
 
-def _write_archive(path, counts):
+def _read_archive(path):
     try:
         with open(path, "r") as f:
             archive = json.load(f)
     except FileNotFoundError:
         archive = []
+    return archive
+
+
+def _write_archive(path, archive):
+    with open(path, "w") as f:
+        json.dump(archive, f, indent=4)
+
+
+def _append(archive, count):
     today = date.today().strftime("%Y-%m-%d")
     if today not in {entry["date"] for entry in archive}:
         prev_count = sum(map(lambda d: d["downloads"], archive))
-        archive.append({"date": today, "downloads": counts - prev_count})
-    with open(path, "w") as f:
-        json.dump(archive, f, indent=4)
+        archive.append({"date": today, "downloads": count - prev_count})
 
 
 def count_github(user, package, path):
     """Download count for the GitHub repository."""
     repo = Github().get_repo(f"{user}/{package}")
-    total = sum(sum(map(lambda d: d.download_count, release.get_assets())) for release in repo.get_releases())
-    _write_archive(path, total)
-    return total
+    count = sum(sum(map(lambda d: d.download_count, release.get_assets())) for release in repo.get_releases())
+    archive = _read_archive(path)
+    _append(archive, count)
+    _write_archive(path, archive)
+    return count
 
 
 def count_conda(package, path):
     """Download count for the Anaconda distribution."""
-    total = int(condastats.cli.overall(package))
-    _write_archive(path, total)
-    return total
+    count = int(condastats.cli.overall(package))
+    archive = _read_archive(path)
+    _append(archive, count)
+    _write_archive(path, archive)
+    return count
 
 
 def count_pypi(package, path):
     """Download count for the PyPI distribution."""
     df = pypistats.overall(package, total=True, format="pandas")
-    with open(path, "r") as f:
-        archive = json.load(f)
+    archive = _read_archive(path)
     for _, data in df.iterrows():
         if data["category"] != "Total" and all([d["category"] != data["category"] or d["date"] != data["date"] for d in archive]):
             data_util = data.to_dict()
             data_util.pop("percent")
             archive.append(data_util)
     archive.sort(key=lambda x: f"{x['category']}{x['date']}")
-    with open(path, "w") as f:
-        json.dump(archive, f, indent=4)
+    _write_archive(path, archive)
     return sum(d["downloads"] for d in archive if d["category"] == "without_mirrors")
 
 
 if __name__ == "__main__":
     archives = Path("archives").resolve(True)
-    with open(archives / "total.json", "w") as f:
-        json.dump({
-            "github": count_github("cobyqa", "cobyqa", archives / "github.json"),
-            "pypi": count_pypi("cobyqa", archives / "pypi.json"),
-        }, f, indent=4)
+    _write_archive(archives / "total.json", {
+        "github": count_github("cobyqa", "cobyqa", archives / "github.json"),
+        "pypi": count_pypi("cobyqa", archives / "pypi.json"),
+    })
